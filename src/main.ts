1d57fd9ce5eff7480715fa8453c64e69175972b1
andrew <urgumandrei@gmail.com>
2023-11-20 15:52:56 +0200

    CORS update

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,20 @@ import {
   NestFastifyApplication,
   FastifyAdapter,
 } from '@nestjs/platform-fastify';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  app.enableCors();
+  app.use(
+    cors({
+      credentials: true,
+      origin: '*',
+    }),
+  );
+  // app.enableCors();
   await app.listen(3030);
 }
 bootstrap();
