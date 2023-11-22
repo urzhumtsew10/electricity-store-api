d4670fb067b7400c907dcc869b6e2ef429c00ff1
andrew <urgumandrei@gmail.com>
2023-11-22 14:45:33 +0200

    fix errors

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,7 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
-  const whitelist = ['http://localhost:3000'];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(3030);
 }
